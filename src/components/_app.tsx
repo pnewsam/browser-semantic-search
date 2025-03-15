1f942e4d935e84c13e3310eb08c537907eea84cb
pnewsam <paul.newsam@gmail.com>
2025-03-15 12:44:38 -0700

    Rename entry

--- src/components/_app.tsx
+++ src/components/_app.tsx
@@ -4,7 +4,9 @@ export default function App() {
   return (
     <main className="bg-stone-100 min-h-screen">
       <div className="container max-w-screen-md mx-auto px-4 py-8">
-        <h1 className="text-2xl font-bold mb-4">Browser Semantic Search</h1>
+        <h1 className="text-2xl font-bold tracking-tight mb-4">
+          Browser Semantic Search
+        </h1>
         <p className="text-zinc-600 mb-4">
           This is a simple semantic search. It creates embeddings of the text of
           the page, and then when the user searches, it creates an embedding of
